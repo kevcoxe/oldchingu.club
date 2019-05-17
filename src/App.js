194963cd6dbe8bc0c7bfae70af048f8ee336cfa8
Kevin Coxe <kcoxe@attilasec.com>
2019-05-17 07:55:38 -0400

    show instagram followrs

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
-import Gallery from 'react-grid-gallery';
 
-function importAll(r) {
-  return r.keys().map(r);
-}
-
-const images = importAll(require.context('./pics', false, /\.(png|jpe?g|svg)$/));
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loadingFollowers: true,
+      followers: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.getFollerCount()
+  }
+
+  getFollerCount() {
+    const that = this;
+    var url = "https://www.instagram.com/oldchingu";
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function () {
+        if (xhr.status === 200 && xhr.responseText.indexOf(("meta property=\"og:description\" content=\"")) !== -1) {
+            var followers = xhr.responseText.split("meta property=\"og:description\" content=\"")[1].split("Followers")[0];
+            console.log("followers:", followers);
+            that.setState({
+              followers: followers,
+              loadingFollowers: false,
+            })
+        }
+    };
+    xhr.send();
+  }
+
   render() {
-    const pics = images.map((image) => {
-      return {
-        src: image,
-        thumbnail: image,
-        isSelected: false,
-      }
-    })
+
+    const { followers, loadingFollowers } = this.state;
+
+    const instagram = loadingFollowers ? <h1>Loading instagram followers</h1> : <h1>Instagram followers: { followers }</h1>;
 
     return (
       <div className="App">
-        <Gallery
-          images={pics}
-          enableLightbox={false}
-          enableImageSelection={false}
-        />
+        { instagram }
       </div>
-    );
+    )
+
   }
 }
 
