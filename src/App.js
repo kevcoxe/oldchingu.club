525e22d2df7287850339c5f6d8584db62f8f76ac
Kevin Coxe <kcoxe@attilasec.com>
2019-05-30 08:07:47 -0400

    update form

--- src/App.js
+++ src/App.js
@@ -39,12 +39,15 @@ class App extends Component {
     return (
       <div className="App">
         { instagram }
-        <form name="contact" netlify>
+        <form name="contact" method="POST" data-netlify="true" action="/test">
           <p>
-            <label>Name <input type="text" name="name" /></label>
+            <label>Your Name: <input type="text" name="name" /></label>   
           </p>
           <p>
-            <label>Email <input type="email" name="email" /></label>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
           </p>
           <p>
             <button type="submit">Send</button>
